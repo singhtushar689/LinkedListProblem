e88a75be8509c5d72f656a4f5f8225916d957467
singhtushar689 <Singhtushar689@gmail.com>
2023-02-23 23:05:22 +0530

    [Tushar] add:Creating a Simple LinkedList

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine( "{0} inserted in the Linkedlist",node.data);
+
+        }
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Welcome to LinkedList Problems");
+            bool flag = true;
+            while (flag) 
+            {
+                Console.WriteLine("Enter Option Given Below: \n 1. Add SimpleLinkedList");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    default:
+                        Console.WriteLine("Enter any Valid Option");
+                        break;
+                }
+            }
+            
+
+
+        }
+    }
+}
